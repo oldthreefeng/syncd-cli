aad039ffa519e11b3fd6bfb3770c22d87dbd3155
Louis <louisehong4168@gmail.com>
2019-10-20 18:14:26 +0800

    fix bash

--- push.sh
+++ push.sh
@@ -7,8 +7,8 @@ read name
 git commit -m "$name"
 
 git push origin master
-git remote move origin
+git remote remove origin
 git remote add https://github.com/oldthreefeng/syncd-cli.git
 git push origin master && \
-git remote move origin
+git remote remove origin
 git remote add git@gogs.wangke.co:go/syncd-cli.git
